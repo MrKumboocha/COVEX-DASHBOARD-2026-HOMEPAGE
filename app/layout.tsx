8d9d06c20f687c53f15692c30f552c8404b7a00e
MrKumboocha <elijahchadwick@gmail.com>
2026-04-20 15:19:16 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Covex',
+  description: 'Covex homepage',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
