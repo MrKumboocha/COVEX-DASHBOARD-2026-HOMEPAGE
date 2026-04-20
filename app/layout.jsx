42795e53cac7046a9d5904edcc278ee15bafbefb
MrKumboocha <elijahchadwick@gmail.com>
2026-04-20 15:29:46 -0400

    Update and rename layout.tsx to layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,11 +3,7 @@ export const metadata = {
   description: 'Covex homepage',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
